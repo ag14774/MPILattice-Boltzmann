ed1ef105c021bdfbfd3bc6db6edb8dfc2a189fba
Andreas Georgiou <ag14774@newblue2.cm.cluster>
2016-10-20 11:47:19 +0100

    Small optimisations in accelerate flow. Changed WAIT_POLICY to ACTIVE

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -248,20 +248,24 @@ int accelerate_flow(const t_param params, t_speed* cells1, int* obstacles1,int t
   {
     /* if the cell is not occupied and
     ** we don't send a negative density */
-    if (!obstacles1[ii * params.nx + jj]
-        && (cells1[ii * params.nx + jj].speeds[3] - w1) > 0.0
-        && (cells1[ii * params.nx + jj].speeds[6] - w2) > 0.0
-        && (cells1[ii * params.nx + jj].speeds[7] - w2) > 0.0)
-    {
-
-      /* increase 'east-side' densities */
-      cells1[ii * params.nx + jj].speeds[1] += w1;
-      cells1[ii * params.nx + jj].speeds[5] += w2;
-      cells1[ii * params.nx + jj].speeds[8] += w2;
-      /* decrease 'west-side' densities */
-      cells1[ii * params.nx + jj].speeds[3] -= w1;
-      cells1[ii * params.nx + jj].speeds[6] -= w2;
-      cells1[ii * params.nx + jj].speeds[7] -= w2;
+    if (!obstacles1[ii * params.nx + jj]){
+      double res1 = cells1[ii * params.nx + jj].speeds[3] - w1;
+      if(res1>0.0){
+        double res2 = cells1[ii * params.nx + jj].speeds[6] - w2;
+        if(res2>0.0){
+          double res3 = cells1[ii * params.nx + jj].speeds[7] - w2;
+          if(res3>0.0){
+             /* increase 'east-side' densities */
+            cells1[ii * params.nx + jj].speeds[1] += w1;
+            cells1[ii * params.nx + jj].speeds[5] += w2;
+            cells1[ii * params.nx + jj].speeds[8] += w2;
+            /* decrease 'west-side' densities */
+            cells1[ii * params.nx + jj].speeds[3] = res1;
+            cells1[ii * params.nx + jj].speeds[6] = res2;
+            cells1[ii * params.nx + jj].speeds[7] = res3;
+          }
+        }
+      }
     }
   }
 
